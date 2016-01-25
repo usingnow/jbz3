dd80c76d77430c1eef936f0b2ca8c6094c007615
Jimmy Xu <jianmin.xu@usingnow.com>
2016-01-25 11:40:27 +0800

    revise the title of the jbz sku and product.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,16 @@ Product.create!(name:               '“爱易分”积分返值保障计划',
                 insurance_cycle:    1,
                 age_starts_at:      18,
                 age_ends_at:        65,
-                benifit:            '理财产品的好处，理财产品的好处，理财产品的好处，理财产品的好处，理财产品的好处，理财产品的好处，理财产品的好处，理财产品的好处。',
-                acknowledgement:    '保障须知', 
-                recommendation:     '产品解读')
+                benifit:            '',
+                acknowledgement:    '', 
+                recommendation:     '')
 
 period     = ['即时版','季度版','半年版','年度版']
 redemption = [0, 90, 180, 360]
 interests  = [0, 0.2, 0.5, 1]
 
 (1..4).each do |n|
-  JbzSku.create!(title:             '爱易分“积分储蓄”年金返还保障计划（' + period[n-1] + '）',
+  JbzSku.create!(title:             '“爱易分”积分返值保障计划（' + period[n-1] + '）',
                  price:             100.00 + interests[n-1],
                  original_price:    100.00 + interests[n-1],
                  reward:            102000,
