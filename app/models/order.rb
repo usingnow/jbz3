66740aa857185cbe3991b3107e58003c3db3f3aa
SpiderEvgn <xiajun.zhang@usingnow.com>
2016-01-25 22:05:23 +0800

    change class of "select/bank"

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,7 @@ class Order < ActiveRecord::Base
   has_many :request_dynamic_passwords
   has_many :line_items, dependent: :destroy
 
-  validates :name, :cellphone, :id_card, :creditcard_num, :debitcard_num, presence: true
+  validates :name, :cellphone, :id_card, :creditcard_num, :debitcard_num, :bank, presence: true
   # validates :id_card, format: { with: /([1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3})|([1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{4})/, message: "身份证号不正确，请重新输入" }
   validates :cellphone, format: { with: /1(3\d|5[^4]|8[^34])\d{8}/, message: "手机号不正确，请重新输入" }, length: { is: 11 }
 
