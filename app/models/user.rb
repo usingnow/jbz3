a90288bd59aeb0b411cb1972d5688a5aad94d20a
SpiderEvgn <xiajun.zhang@usingnow.com>
2016-01-25 18:26:05 +0800

    add user validation; fix a bug in devise:yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,14 @@ class User < ActiveRecord::Base
          :rememberable, :trackable, :validatable, :authentication_keys => [:login]
   
   validates_presence_of :cellphone, :creditcard_num
-  validates_presence_of :id_card, :name, on: :update
-  validates_uniqueness_of :cellphone
-  validates :id_card, format: { with: /([1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3})|([1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{4})/, message: "身份证号不正确，请重新输入" }, on: :update
+  validates_uniqueness_of :cellphone, :creditcard_num
   validates :cellphone, format: { with: /1(3\d|5[^4]|8[^34])\d{8}/, message: "手机号不正确，请重新输入" }, length: { is: 11 }
   validates_length_of :creditcard_num, is: 16, message: "浦发信用卡号不正确，请重新输入"
-  
+
+  # update 时需要验证的字段
+  validates :id_card, format: { with: /([1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3})|([1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{4})/, message: "身份证号不正确，请重新输入" }, on: :update
+  validates_length_of :id_card, maximum: 18, on: :update
+  validates_presence_of :id_card, :name, on: :update
   
 
   # 取消 devise 默认 email 必须有值
